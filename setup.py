702dbcda8eae1ab9309aa3df5b916580723f31e3
Daxeel Soni <daxeel_soni4444@yahoo.com>
2015-12-29 10:35:59 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pypixoto',
+      version='1.0',
+      description='Python SDK for accessing the data from pixoto.com',
+      author='Daxeel Soni',
+      author_email='sayhi@daxeelsoni.in',
+      url='http://www.daxeelsoni.in',
+      keywords = ['pypixoto', 'pixoto', 'photography', 'photo', 'photographer'],
+      py_modules=['pypixoto'],
+     )
